370db838758667977b8cb2d0bde0ca818c57ae1c
koval-andrey <kovalandrey1234@gmail.com>
2021-07-28 21:46:03 +0300

    hw-05
    start

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const path = require("path");
+const fs = require("fs/promises");
+const moment = require("moment");
+//const { v4 } = require("uuid");
+const multer = require('multer')
+
 require('dotenv').config()
 require('./configs/passport-config')
 const api = require('./api/auth')
@@ -10,6 +16,30 @@ const { DB_HOST, PORT = 3000 } = process.env
 const router = require('./routes/api/contacts')
 
 const app = express()
+
+const UPLOAD_DIR = path.join(__dirname, process.env.UPLOAD_DIR)
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, UPLOAD_DIR);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+  limits: {
+    fileSize: 2000000,
+  },
+  fileFilter(req, file, cb) {
+    if (file.mimetype.includes("image")) {
+      cb(null, true) return
+    };
+    cb(null, false);
+
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
 
 app.use(cors())
 app.use(express.json())
