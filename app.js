c9fb5a198970ae88cdaea60052bb020fbbbf7862
koval-andrey <kovalandrey1234@gmail.com>
2021-07-29 23:02:14 +0300

    hw-05
    jimp

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const path = require("path");
+const jimp = require('jimp')
 const fs = require("fs/promises");
 const moment = require("moment");
 //const { v4 } = require("uuid");
 const multer = require('multer')
+
 
 require('dotenv').config()
 require('./configs/passport-config')
@@ -13,11 +15,14 @@ const api = require('./api/auth')
 
 const { DB_HOST, PORT = 3000 } = process.env
 
-const router = require('./routes/api/contacts')
+const router = require('./routes/api/contacts');
+const { create } = require('./services/contact');
 
 const app = express()
 
 const UPLOAD_DIR = path.join(__dirname, process.env.UPLOAD_DIR)
+const IMG_DIR = path.join(__dirname, 'public', 'images')
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, UPLOAD_DIR);
@@ -36,13 +41,57 @@ const storage = multer.diskStorage({
 
   },
 });
+app.use(cors())
+app.use(express.json())
 
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.post('/upload', upload.single('avatar'), async (req, res, next) => {
+  console.log(req.file)
+  console.log(req.body)
+  if (req.file) {
+    const { file } = req
+    const img = await jimp.read(file.path)
+    await img.autocrop()
+      .cover(250, 250, jimp.HORIZONTAL_ALIGN_CENTER || jimp.VERTICAL_ALIGN_MIDDLE)
+      .writeAsync(file.path)
+    await fs.rename(file.path, path.join(IMG_DIR, file.originalname)
+  }
+  res.redirect('/')
+})
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 const upload = multer({
   storage: storage,
 });
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.json({ message: err.message, status: err.status })
+})
 
-app.use(cors())
-app.use(express.json())
+const isAccessible = (path) => {
+  return fs
+    .access(path)
+    .then(() => true)
+    .catch(() => false)
+}
+
+const createFolderIsNotExist = async (folder) => {
+  if (!(await isAccessible(folder))) {
+    await fs.mkdir(folder)
+  }
+}
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, async () => {
+  createFolderIsNotExist(UPLOAD_DIR)
+  createFolderIsNotExist(IMG_DIR)
+  console.log(`Server running. Use on port:${PORT}`)
+})
+
 
 app.use('/api/contacts', router)
 app.use('/api/auth', api.auth)
